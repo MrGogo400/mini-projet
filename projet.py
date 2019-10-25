f2ff755cb78d525c096a9ba2f8d68060f3590a88
Hugo <play.craft@hotmail.fr>
2019-10-25 20:32:06 +0200

    faute de frappe

--- projet.py
+++ projet.py
@@ -18,7 +18,7 @@ def calcul(distance):
     temps = distance / 90
     heures = int(temps)
     minutes = int((temps-heures) * 60)
-    freinaccel = 18 # Temps que prend le camion pour s'arreter et accelerrer 
+    freinaccel = 18 # Temps que prend le camion pour s'arreter et accelerer 
     tempsdepause = 15
 
     pause = heures/2 + (freinaccel + tempsdepause)
